2afb940293ff73e96d3d4faaca556b80c3cc2345
temy13 <temy.poporofier00@gmail.com>
2014-11-27 14:23:39 +0900

    pdf file convert to jpg

---
+++ ARKeyBoard/Assets/Plugins/SceneScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneScript : MonoBehaviour {
+
+	//public GameObject KeyBoard;
+	public string log;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(this);
+		//Application.LoadLevel("KeyBoardWithScore");
+		log = "test";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
